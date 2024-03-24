734deb0ec018664c64e1f214876971d979a18156
jakubdrobny <jkubeing@gmail.com>
2024-03-24 13:29:40 +0100

    add basic navbar and online-competitions route with mock data

---
+++ frontend/src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="welcome-title">Welcome to Speedcubing Slovakia!</h1>
+        </div>
+    );
+}
+
+export default Home;
