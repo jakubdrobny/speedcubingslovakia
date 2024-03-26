0ecb6f2ffbcd7dec31f98bf4fc83819a4b5c1795
jakubdrobny <jkubeing@gmail.com>
2024-03-26 11:51:38 +0100

    fix twisty-player not showing

--- frontend/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { HTMLAttributes } from "react";
-
-declare global {
-    namespace JSX {
-      interface IntrinsicElements {
-        "scramble-display": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      }
-    }
-  }
