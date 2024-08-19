ec19a9692405417bafed59dff8e7f85c2086f5ad
jakubdrobny <jkubeing@gmail.com>
2024-08-19 15:23:13 +0200

    wip actually making cronjob work

---
+++ backend/cronjob/cronjob.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jakubdrobny/speedcubingslovakia/backend/controllers"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	envMap, err := godotenv.Read(fmt.Sprintf(".env.%s", os.Getenv("SPEEDCUBINGSLOVAKIA_BACKEND_ENV")))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to load enviromental variables from file: %v\n", err)
+		os.Exit(1)
+	}
+
+	db, err := pgxpool.New(context.Background(), envMap["DB_URL"])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	controllers.AddNewWeeklyCompetition(db, envMap)
+}
