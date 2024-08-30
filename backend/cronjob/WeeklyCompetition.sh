ff65aaa663a2114853d271f530fd072495791f73
jakubdrobny <jkubeing@gmail.com>
2024-08-30 10:36:14 +0200

    improving loggin in WeeklyCompetition.sh

--- backend/cronjob/WeeklyCompetition.sh
+++ backend/cronjob/WeeklyCompetition.sh
@@ -3,5 +3,9 @@
 set -o allexport
 . ~/.profile
 set +o allexport
+
 cd $SPEEDCUBINGSLOVAKIA_PATH/backend
+
+echo "===== START: `date` =====" >> $SPEEDCUBINGSLOVAKIA_CRONJOB_LOGFILE_PATH/WeeklyCompetitionJob.txt 2>&1
 go run cronjob/WeeklyCompetitionJob.go >> $SPEEDCUBINGSLOVAKIA_CRONJOB_LOGFILE_PATH/WeeklyCompetitionJob.txt 2>&1
+echo "===== END =====" >> $SPEEDCUBINGSLOVAKIA_CRONJOB_LOGFILE_PATH/WeeklyCompetitionJob.txt 2>&1
