333cfc22b7438fe79efe05d73427dd7737415d66
jakubdrobny <jkubeing@gmail.com>
2024-08-30 10:08:49 +0200

    fix cronjob sh script

--- backend/cronjob/WeeklyCompetition.sh
+++ backend/cronjob/WeeklyCompetition.sh
@@ -3,5 +3,5 @@
 set -o allexport
 . ~/.profile
 set +o allexport
-cd /home/jakubd/skola/Tvorba-internetovych-aplikacii/speedcubingslovakia/backend
-go run cronjob/WeeklyCompetitionJob.go >> /home/jakubd/cronjob_tmp_output/speedcubingslovakia_output.txt 2>&1
+cd ~/skola/Tvorba-internetovych-aplikacii/speedcubingslovakia/backend
+go run cronjob/WeeklyCompetitionJob.go >> ~/cronjob_tmp_output/WeeklyCompetitionJob.txt 2>&1
