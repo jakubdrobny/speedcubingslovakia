371f7784de91a7658f052e2c0343f9f61c29979f
jakubdrobny <jkubeing@gmail.com>
2024-09-12 15:07:41 +0200

    wip trying to find a good design of the map

--- frontend/src/components/Users/Users.tsx
+++ frontend/src/components/Users/Users.tsx
@@ -1,32 +1,33 @@
 import {
-  Alert,
-  Button,
-  Card,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Stack,
-  Typography,
-} from "@mui/joy";
+  ComposableMap,
+  Geographies,
+  Geography,
+  Graticule,
+  Sphere,
+  ZoomableGroup,
+} from "react-simple-maps";
 import { LoadingState, SearchUser } from "../../Types";
-import {
-  getError,
-  getUsers,
-  initialLoadingState,
-  isObjectEmpty,
-  renderResponseError,
-} from "../../utils/utils";
+import { Stack, Typography } from "@mui/joy";
+import { getError, getUsers, initialLoadingState } from "../../utils/utils";
+import { useEffect, useState } from "react";
 
-import { Link } from "react-router-dom";
-import { Search } from "@mui/icons-material";
-import { useState } from "react";
+import { Tooltip } from "react-tooltip";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3-scale";
+
+const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+// const geoUrl = "/features.json";
+
+const colorScale = scaleLinear<string, string>()
+  .domain([0.29, 0.68])
+  .range(["#ffedea", "#ff5233"]);
 
 const Users = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [loadingState, setLoadingState] =
     useState<LoadingState>(initialLoadingState);
   const [users, setUsers] = useState<SearchUser[]>([]);
+  const [tooltipContent, setTooltipContent] = useState("");
 
   const searchForUsers = () => {
     setLoadingState({ isLoading: true, error: {} });
@@ -41,46 +42,53 @@ const Users = () => {
       });
   };
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    csv(`${process.env.PUBLIC_URL}/vulnerability.csv`).then((data: any) => {
+      setData(data);
+    });
+  }, []);
+
   return (
-    <Stack sx={{ margin: "1em" }} spacing={2}>
-      <Typography level="h2">Find users</Typography>
-      <FormControl disabled={loadingState.isLoading}>
-        <FormLabel>Enter WCA ID or username:</FormLabel>
-        <Input
-          placeholder="Enter WCA ID or username... (eg. 2016DROB01)"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyDown={(e) => (e.key === "Enter" ? searchForUsers() : null)}
-          autoFocus
-          startDecorator={
-            <Button
-              startDecorator={<Search />}
-              variant="soft"
-              color="neutral"
-              onClick={searchForUsers}
-            >
-              Search
-            </Button>
-          }
-        />
-        <FormHelperText>
-          Leave empty for all users. Matches WCA ID exactly, but name as a part
-          case insensitive.
-        </FormHelperText>
-      </FormControl>
-      {!isObjectEmpty(loadingState.error)
-        ? renderResponseError(loadingState.error)
-        : users.map((u: SearchUser) => (
-            <Card sx={{ display: "flex", flexDirection: "row" }}>
-              <span style={{ fontWeight: "bold" }}>{u.username}: </span>
-              <Link
-                to={`/profile/${u.wcaid}`}
-                style={{ textDecoration: "none" }}
-              >
-                {u.wcaid}
-              </Link>
-            </Card>
-          ))}
+    <Stack sx={{ margin: "1em" }}>
+      <Typography level="h2">Users</Typography>
+      <div data-tooltip-id="my-tooltip" data-tooltip-content="">
+        <ComposableMap
+          projectionConfig={{
+            scale: 147,
+          }}
+        >
+          <ZoomableGroup>
+            <Sphere
+              id="sphere-id"
+              fill="transparent"
+              stroke="#E4E5E6"
+              strokeWidth={1}
+            />
+            <Graticule stroke="#E4E5E6" strokeWidth={1} />
+            {data.length > 0 && (
+              <Geographies geography={geoUrl}>
+                {({ geographies }) =>
+                  geographies.map((geo) => {
+                    const d = data.find((s: any) => s.ISO3 === geo.id);
+                    return (
+                      <Geography
+                        key={geo.rsmKey}
+                        geography={geo}
+                        fill={"#0B6BCB"}
+                        stroke="white"
+                        strokeWidth={1}
+                      />
+                    );
+                  })
+                }
+              </Geographies>
+            )}
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+      <Tooltip id="my-tooltip">{tooltipContent}</Tooltip>
     </Stack>
   );
 };
