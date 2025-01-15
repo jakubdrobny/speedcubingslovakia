ba3117a6b0d7ad605770f3ce4c11d2c5cc483b81
jakubdrobny <jkubeing@gmail.com>
2025-01-15 01:13:16 +0100

    remove useless migration

--- database/migrations/000008_unique_newsletter_country_state_user.up.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE wca_competitions_announcements_subscriptions DROP CONSTRAINT wca_competitions_announcements_subscript_country_id_user_id_key;
-ALTER TABLE wca_competitions_announcements_subscriptions ADD CONSTRAINT wca_competitions_announcements_subscript_country_id_state_user_id_key UNIQUE (country_id, state, user_id);
-
-COMMIT;
