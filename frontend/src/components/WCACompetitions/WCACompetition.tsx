674fa82e020d05e2fe4cb433c672a61a541a2b00
jakubdrobny <jkubeing@gmail.com>
2025-07-13 19:16:28 +0200

    add wcalive link to currently running competitions and fix live chip

--- frontend/src/components/WCACompetitions/WCACompetition.tsx
+++ frontend/src/components/WCACompetitions/WCACompetition.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, Chip, Divider, Stack, Typography } from "@mui/joy";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CompetitionEvent, WCACompetitionType } from "../../Types";
 import {
@@ -10,11 +10,74 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
+const wcaLiveAPIBody = (name: string) => {
+  return {
+    operationName: "Competitions",
+    variables: {
+      from: "2025-06-13",
+      name: name,
+    },
+    query: `query Competitions($from: Date!, $name: String!) {\n  competitions(from: $from, filter: $name) {\n    id\n    name\n    startDate\n    endDate\n    startTime\n    endTime\n    venues { \n      id\n      country { \n        iso2\n        __typename\n }\n
+__typename\n    }\n    __typename\n  }\n  recentRecords { \n    ...records\n    __typename\n } \n}\n\nfragment records on Record {
+\n  id\n
+  tag\n  type\n  attemptResult\n  result {
+  \n    id\n    person {
+  \n      id\n      name\n      country {
+  \n        iso2\n        name\n
+    __typename\n
+  } \n      __typename\n
+  } \n    round {
+    \n      id\n      competitionEvent {
+    \n        id\n        event {
+    \n
+      id\n          name\n          __typename\n
+    } \n        competition { \n          id\n          __typename\n } \n        __typename\n
+    } \n      __typename\n
+    } \n    __typename\n
+  } \n  __typename\n
+}`
+  }
+};
+
 const WCACompetition: React.FC<{ comp: WCACompetitionType }> = ({ comp }) => {
+  const [wcaLiveId, setWcaLiveId] = useState("");
+  const isLive = dayjs(comp.startdate).isBefore(dayjs());
+
+  useEffect(() => {
+    const fetchWCALiveCompInfo = async () => {
+      fetch('https://live.worldcubeassociation.org/api', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(wcaLiveAPIBody(comp.name)),
+      }).then(response => {
+        return response.json();
+      }).then(data => {
+        if (!data || !data.data) return;
+        if (!data.data.competitions) {
+          console.log('competitions are not in the response');
+          return;
+        }
+        if (data.data.competitions.length === 0) {
+          console.log('competition not found: ' + comp.name)
+          return;
+        }
+        const wcaLiveCompetition = data.data.competitions[0];
+        setWcaLiveId(wcaLiveCompetition.id)
+      }).catch(err => {
+        console.log(err)
+      });
+
+    };
+
+    fetchWCALiveCompInfo();
+  }, []);
+
   return (
     <Stack component={Card} direction="column">
       <Stack spacing={1} direction="row">
-        {dayjs(comp.startdate).isBefore(dayjs(comp.startdate)) && (
+        {isLive && (
           <Chip color="success">Live!</Chip>
         )}
         <Typography level="h3">{comp.name}</Typography>
@@ -92,21 +155,34 @@ const WCACompetition: React.FC<{ comp: WCACompetitionType }> = ({ comp }) => {
         <Typography>
           <b>Events:</b>
         </Typography>
-        {comp.events.map((event: CompetitionEvent, idx2: number) => (
+        {comp.events.sort((e1: CompetitionEvent, e2: CompetitionEvent) => e1.id - e2.id).map((event: CompetitionEvent, idx2: number) => (
           <span
             key={idx2 + 100000}
             className={`${getCubingIconClassName(
               event.iconcode,
-            )} profile-cubing-icon-mock`}
+            )} profile - cubing - icon - mock`}
           />
         ))}
       </Stack>
-      {dayjs(comp.startdate).isBefore(dayjs()) && (
+      {isLive && (
         <Typography>
           <b>Group assignments:</b>&nbsp;
           {
             <Link
               to={"https://competitiongroups.com/competitions/" + comp.id}
+              style={{ color: "#0B6BCB", textDecoration: "none" }}
+            >
+              Link
+            </Link>
+          }
+        </Typography>
+      )}
+      {isLive && (
+        <Typography>
+          <b>Live results:</b>&nbsp;
+          {
+            <Link
+              to={"https://live.worldcubeassociation.com/competitions/" + wcaLiveId}
               style={{ color: "#0B6BCB", textDecoration: "none" }}
             >
               Link
