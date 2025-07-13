0d8e0fbca64f1ae64ccf041f86e1939828fc1089
jakubdrobny <jkubeing@gmail.com>
2025-07-13 22:28:59 +0200

    fix penalties in timer mode

--- frontend/src/context/CompetitionContext.tsx
+++ frontend/src/context/CompetitionContext.tsx
@@ -19,6 +19,7 @@ import {
   initialCompetitionState,
   initialCurrentResults,
   sendResults,
+  reformatWithPenalties
 } from "../utils/utils";
 
 import useState from "react-usestateref";
@@ -154,9 +155,16 @@ export const CompetitionProvider: React.FC<{ children?: ReactNode }> = ({
     setCompetitionState({ ...competitionState, currentSolveIdx: idx });
 
   const saveResults = async (): Promise<void> => {
+    let results = { ...currentResultsRef.current };
+    results.solve1 = reformatWithPenalties(currentResultsRef.current.solve1, competitionState.penalties[0]);
+    results.solve2 = reformatWithPenalties(currentResultsRef.current.solve2, competitionState.penalties[1]);
+    results.solve3 = reformatWithPenalties(currentResultsRef.current.solve3, competitionState.penalties[2]);
+    results.solve4 = reformatWithPenalties(currentResultsRef.current.solve4, competitionState.penalties[3]);
+    results.solve5 = reformatWithPenalties(currentResultsRef.current.solve5, competitionState.penalties[4]);
+
     try {
       const resultEntry = await sendResults({
-        ...currentResultsRef.current,
+        ...results,
         competitionid: competitionState.id,
       });
       if (!resultEntry.status.approvalFinished) {
@@ -165,6 +173,10 @@ export const CompetitionProvider: React.FC<{ children?: ReactNode }> = ({
         setWarningModalOpen(true);
       }
       setCurrentResults(resultEntry);
+      setCompetitionState((ps) => ({
+        ...ps,
+        penalties: Array(5).fill('0')
+      }));
       return Promise.resolve();
     } catch (e) {
       return Promise.reject(e);
